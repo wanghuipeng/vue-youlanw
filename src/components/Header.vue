d79384046a3f236d531fd763433d8c76e49b1c01
wanghuipeng <muslim.wang@dl-kj.com>
2017-07-31 18:03:36 +0800

    首页提交

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="header">
+  	<div class="header">
+			<a href="http://m.youlanw.com/"><img src="http://m.youlanw.com/static/images/logo.png" alt="优蓝网" class="logo"></a>
+			<div class="linkage-select">
+				<div class="form-g-each-r overflow-hide change" >
+					<a href="javascript:void(0)">上海<i class="arrow-d"></i></a>
+				</div>
+			</div>
+			<a href="javascript:;" class="icon-search">搜索</a>
+			<a href="javascript:;" class="icon-menu">导航</a>
+		</div>
+  </div>
+</template>
+<script>
+  export default{
+    data(){
+      return {
+      
+      }
+    },
+    mounted(){
+      
+    },
+    methods: {
+      
+    }
+  };
+</script>
+<style scoped>
+  
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <v-header></v-header>
+    <v-banner></v-banner>
+  </div>
+</template>
+
+<script>
+	import vHeader from './Header.vue'
+	import vBanner from './Banner.vue'
+	export default{
+    components: {vHeader,vBanner},
+    data(){
+      return {
+        
+      }
+    },
+    mounted(){
+
+    },
+    updated(){
+    },
+    destroyed(){
+    }
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
