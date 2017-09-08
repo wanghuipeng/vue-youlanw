0aae2c753ef7a19e17455bd52304111be97f69d2
wanghuipeng <624793769@qq.com>
2017-09-08 15:55:59 +0800

    api修改

--- src/store/api.js
+++ src/store/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // 使用代理
-const HOST =process.env.NODE_ENV === 'production' ? '不告诉你' : 'http://10.0.11.44:8080/api';
+const HOST =process.env.NODE_ENV === 'production' ? '不告诉你' : 'http://10.0.11.44:8080';
 
 export const API_TYPE = {
   movie: {
@@ -24,26 +24,26 @@ export function fetch(url) {
 
 //首页轮播
 export function fetchBannersByType(appkey, client_id, token , branch_id) {
-  return fetch(`/v5/adv/homeBannerByBranchId?appkey=${appkey}&client_id=${client_id}&token=${token}&branch_id=${branch_id}`)
+  return fetch(`/api/v5/adv/homeBannerByBranchId?appkey=${appkey}&client_id=${client_id}&token=${token}&branch_id=${branch_id}`)
 }
 //首页推荐
 export function fetchRecommendByType(appkey, client_id, token , branch_id) {
-  return fetch(`/v5/adv/homeBannerByBranchId?appkey=${appkey}&client_id=${client_id}&token=${token}&branch_id=${branch_id}`)
+  return fetch(`/api/v5/adv/homeBannerByBranchId?appkey=${appkey}&client_id=${client_id}&token=${token}&branch_id=${branch_id}`)
 }
 //首页职位列表
 export function fetchJobListByType(appkey, client_id, token , branch_id) {
-  return fetch(`/v5/adv/homePageByBranchId?appkey=${appkey}&client_id=${client_id}&token=${token}&branch_id=${branch_id}`)
+  return fetch(`/api/v5/adv/homePageByBranchId?appkey=${appkey}&client_id=${client_id}&token=${token}&branch_id=${branch_id}`)
 }
 //职位搜索
 export function fetchSearchListByType(appkey, client_id, token , keyword) {
-  return fetch(`/v4/enterprise/findByKeyword?appkey=${appkey}&client_id=${client_id}&token=${token}&keyword=${keyword}`)
+  return fetch(`/api/v4/enterprise/findByKeyword?appkey=${appkey}&client_id=${client_id}&token=${token}&keyword=${keyword}`)
 }
 //说说列表
 export function fetchChatListByType(appkey, client_id, token , page , limit) {
-  return fetch(`/v6/article/ramble?appkey=${appkey}&client_id=${client_id}&token=${token}&page=${page}&limit=${limit}`)
+  return fetch(`/api/v6/article/ramble?appkey=${appkey}&client_id=${client_id}&token=${token}&page=${page}&limit=${limit}`)
 }
 //说说详情
 export function fetchChatDetailByType(appkey, client_id, token , id) {
-  return fetch(`/v6/article/detail?appkey=${appkey}&client_id=${client_id}&token=${token}&id=${id}`)
+  return fetch(`/api/v6/article/detail?appkey=${appkey}&client_id=${client_id}&token=${token}&id=${id}`)
 }
 //添加评论
